9eb0ee2f193a2f2010d81e7fb71f26cf2a85ebe9
fbobg05 <28024559+fbobg05@users.noreply.github.com>
2018-02-11 13:32:54 +0200

    Added Nunit

---
+++ NBU.WorkoutTracker.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NBU.WorkoutTracker.Tests
+{
+    [TestFixture]
+    public class ShouldRun
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(1==1);
+        }
+    }
+}
