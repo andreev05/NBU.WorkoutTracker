fa3b793b6e49c9f94989412ed23550e60736e056
fbobg05 <28024559+fbobg05@users.noreply.github.com>
2018-02-11 12:08:04 +0200

    Implemented repository pattern

--- NBU.WorkoutTracker.Infrastructure/Data/Contracts/IRDBRepository.cs
+++ NBU.WorkoutTracker.Infrastructure/Data/Contracts/IRDBRepository.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Linq;
 
 namespace NBU.WorkoutTracker.Infrastructure.Data.Contracts
 {
-    interface IRDBRepository
+    /// <summary>
+    /// Interface for RDB Repository
+    /// RDB: Relational Database
+    /// </summary>
+    /// <typeparam name="T">Type of the data table to which 
+    /// current reposity is attached</typeparam>
+    public interface IRDBERepository<T> : IRepository<T> where T : class
     {
+        /// <summary>
+        /// The result collection won't be tracked by the context
+        /// </summary>
+        /// <returns>Expression tree</returns>
+        IQueryable<T> AllReadonly();
+
+        /// <summary>
+        /// Detaches given entity from the context
+        /// </summary>
+        /// <param name="entity">Entity to be detached</param>
+        void Detach(T entity);
+
+        /// <summary>
+        /// Saves all made changes in trasaction
+        /// </summary>
+        /// <returns>Error code</returns>
+        int SaveChanges();
     }
 }
