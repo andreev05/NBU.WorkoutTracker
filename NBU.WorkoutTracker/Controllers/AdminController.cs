e2ce9311813d8b99371541f2510744753748d207
fbobg05 <28024559+fbobg05@users.noreply.github.com>
2018-02-11 14:10:24 +0200

    Added controllers

---
+++ NBU.WorkoutTracker/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NBU.WorkoutTracker.Controllers
+{
+    /// <summary>
+    /// The admin can only view the users and create new admins
+    /// </summary>
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NBU.WorkoutTracker/Controllers/PlansController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NBU.WorkoutTracker.Infrastructure.Data.Contexts;
+using NBU.WorkoutTracker.Infrastructure.Data.Models;
+
+namespace NBU.WorkoutTracker.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly WorkoutTrackerDbContext _context;
+
+        public PlansController(WorkoutTrackerDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Plans
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Plans.ToListAsync());
+        }
+
+        // GET: Plans/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans
+                .SingleOrDefaultAsync(m => m.PlanId == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return View(plan);
+        }
+
+        // GET: Plans/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Plans/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PlanId,DateCreated,PlanName")] Plan plan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(plan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // GET: Plans/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans.SingleOrDefaultAsync(m => m.PlanId == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+            return View(plan);
+        }
+
+        // POST: Plans/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PlanId,DateCreated,PlanName")] Plan plan)
+        {
+            if (id != plan.PlanId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlanExists(plan.PlanId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // GET: Plans/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans
+                .SingleOrDefaultAsync(m => m.PlanId == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return View(plan);
+        }
+
+        // POST: Plans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var plan = await _context.Plans.SingleOrDefaultAsync(m => m.PlanId == id);
+            _context.Plans.Remove(plan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlanExists(int id)
+        {
+            return _context.Plans.Any(e => e.PlanId == id);
+        }
+    }
+}
